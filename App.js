05387c468ca4a69385ef1e096ede1470a419b077
Jeztc <dagreenrs@gmail.com>
2022-09-03 20:11:11 +0300

    feat: part 1 exercises

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-import Main from './src/components/Main';
-import {NativeRouter} from 'react-router-native';
-import {StatusBar} from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
-    return (
-        <>
-            <NativeRouter>
-                <Main />
-            </NativeRouter>
-            <StatusBar style="auto" />
-        </>
-    );
-};
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
